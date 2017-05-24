a7613619870a9b5827fefd49eceeb7e327cef434
sunpeng <sunpeng@yanxiu.com>
2017-05-24 22:17:46 +0800

    解决键盘上推问题

--- app/src/main/java/com/example/sunpeng/windowsoftinputmodetest/MainActivity.java
+++ app/src/main/java/com/example/sunpeng/windowsoftinputmodetest/MainActivity.java
@@ -3,20 +3,21 @@ package com.example.sunpeng.windowsoftinputmodetest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private boolean mIsShowing = false;
+    private boolean mKeyBoardVisible = false;
 
     private TextView mBottom;
 
     private EditText mEdit;
 
     private View mContent;
+
+    private int mContentBottom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,34 +25,31 @@ public class MainActivity extends AppCompatActivity {
         mBottom = (TextView) findViewById(R.id.tv_bottom);
         mEdit = (EditText) findViewById(R.id.et);
         mContent = findViewById(R.id.frame_content);
-//        getSupportFragmentManager().beginTransaction().replace(R.id.frame_content,ContentFragment.getInstance()).commit();
-        mEdit.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(!mIsShowing){
-                    mBottom.setVisibility(View.GONE);
-                }
-                return false;
-            }
-        });
-
-        mContent.addOnLayoutChangeListener(mOnLayoutChangeListener);
     }
 
     View.OnLayoutChangeListener mOnLayoutChangeListener = new  View.OnLayoutChangeListener(){
 
         @Override
         public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
-            if(bottom < oldBottom){
-                mIsShowing = true;
-                Log.i("state",String.valueOf(mIsShowing));
-            }else {
-                mIsShowing = false;
-                mContent.removeOnLayoutChangeListener(this);
+            if(mContent.getBottom() < mContentBottom && !mKeyBoardVisible){
+                mKeyBoardVisible = true;
+                mBottom.setVisibility(View.GONE);
+                mContent.requestLayout();
+                Log.i("state",String.valueOf(mKeyBoardVisible));
+            }else if (mContent.getBottom() >= mContentBottom && mKeyBoardVisible){
+                mKeyBoardVisible = false;
                 mBottom.setVisibility(View.VISIBLE);
-                Log.i("state",String.valueOf(mIsShowing));
-                mContent.addOnLayoutChangeListener(this);
+                Log.i("state",String.valueOf(mKeyBoardVisible));
             }
         }
     };
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if(hasFocus){
+            mContentBottom = mContent.getBottom();
+            getWindow().getDecorView().addOnLayoutChangeListener(mOnLayoutChangeListener);
+        }
+    }
 }
